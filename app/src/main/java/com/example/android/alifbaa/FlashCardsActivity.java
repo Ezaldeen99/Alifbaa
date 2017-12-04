9c5d42db0768257a9d9fb87a854c8b45169dca47
emanj <emmasarn93@gmail.com>
2017-12-04 19:00:05 +0300

    adding the intents to main layout

---
+++ app/src/main/java/com/example/android/alifbaa/FlashCardsActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.alifbaa;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.alifbaa.R;
+
+/**
+ * Created by brayan pc on 12/4/2017.
+ */
+
+public class FlashCardsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_cards);
+
+    }
+}
--- app/src/main/java/com/example/android/alifbaa/MainActivity.java
+++ app/src/main/java/com/example/android/alifbaa/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.android.alifbaa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView flashCardGame;
+    ImageView touchTheletterGame;
+    ImageView alphabetTilesGame;
+    ImageView letterTrackingGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //push this to gitlab
+        flashCardGame = (ImageView) findViewById(R.id._1st_game_button);
+        touchTheletterGame = (ImageView) findViewById(R.id._2nd_game_button);
+        alphabetTilesGame = (ImageView) findViewById(R.id._3ed_game_button);
+        letterTrackingGame = (ImageView) findViewById(R.id._4th_game_button);
+        flashCardGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FlashCardsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        touchTheletterGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TouchTheLetterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        alphabetTilesGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlphabetTilesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        letterTrackingGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LetterTrackingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
