08bfb9c9c31f8a26ddb27bf7f67d38643fcecb9b
ali <ali_arsenal95@yahoo.com>
2017-12-13 16:24:00 +0300

    editing alphabet_tiles layout

---
+++ app/src/main/java/com/example/android/alifbaa/Letter.java
@@ -0,0 +1,23 @@
+package com.example.android.alifbaa;
+
+/**
+ * Created by ali_a on 12/12/2017.
+ */
+
+public class Letter {
+    private int indext;
+    private int letterImg;
+
+    public Letter(int indext, int letterImg) {
+        this.indext = indext;
+        this.letterImg = letterImg;
+    }
+
+    public int getIndext() {
+        return indext;
+    }
+
+    public int getLetterImg() {
+        return letterImg;
+    }
+}
