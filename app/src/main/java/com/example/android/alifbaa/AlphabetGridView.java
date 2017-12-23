8cdbf7b6a8d2510909ed5f7dae5c483a6f3c55de
suleiman94 <30908123+suleiman94@users.noreply.github.com>
2017-12-23 09:41:38 +0300

    Merge branch 'sulaiman-branch' of https://gitlab.com/Alifbaa/Alifbaa
    # Conflicts:
    #	app/src/main/java/com/example/android/alifbaa/AlphabetTilesActivity.java
    #	app/src/main/java/com/example/android/alifbaa/alphabetTilesAdapter.java

---
+++ app/src/main/java/com/example/android/alifbaa/AlphabetGridView.java
@@ -0,0 +1,37 @@
+package com.example.android.alifbaa;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Sulaiman on 22/12/2017.
+ */
+
+
+public class AlphabetGridView extends GridView {
+    public AlphabetGridView(Context context) {
+        super(context);
+        this.setNumColumns(5);
+    }
+
+    public AlphabetGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.setNumColumns(5);
+    }
+
+    /**
+     * Set adapter of AlphabetGridView.
+     *
+     * @param adapter Must be an instance of DNDAdapter or will throw.
+     */
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        if (!(adapter instanceof alphabetTilesAdapter)) {
+            throw new IllegalArgumentException("adapter must be an instance of DNDAdapter!");
+        }
+        super.setAdapter(adapter);
+    }
+}
+
---
+++ app/src/main/java/com/example/android/alifbaa/AlphabetTilesViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.android.alifbaa;
+
+/**
+ * Created by sulaiman on 20/12/2017.
+ */
+
+public class AlphabetTilesViewHolder {
+    int position;
+    public AlphabetTilesViewHolder(int position){
+        this.position = position;
+    }
+//    Object data;
+//    ImageView imageView;
+//    TextView textView;
+}
+
