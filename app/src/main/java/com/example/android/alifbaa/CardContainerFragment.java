5347b43201d75f3dd1f319d20a52434bb4f4a79c
azozs <azozsahb99@gmail.com>
2018-02-02 15:12:05 +0300

    // fixing flipping cards and home button

--- app/src/main/java/com/example/android/alifbaa/CardContainerFragment.java
+++ app/src/main/java/com/example/android/alifbaa/CardContainerFragment.java
@@ -12,13 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.Random;
-
 public class CardContainerFragment extends android.app.Fragment {
-    private int position;
-    private boolean cardFlipped = false;
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
     private AudioManager.OnAudioFocusChangeListener mAudioFocusListener = new AudioManager.OnAudioFocusChangeListener() {
         @Override
         public void onAudioFocusChange(int focusChange) {
@@ -37,7 +31,8 @@ public class CardContainerFragment extends android.app.Fragment {
             }
         }
     };
-     void releaseMediaPlayer() {
+
+    void releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
         if (mMediaPlayer != null) {
             mMediaPlayer.release();
@@ -79,102 +74,58 @@ public class CardContainerFragment extends android.app.Fragment {
 
         }
     };
-    private static final int NBR_ITEMS = 30;
-    private int[] AnimalsSounds = {R.raw.obj_1, R.raw.obj_2, R.raw.obj_3, R.raw.obj_4, R.raw.obj_5, R.raw.obj_6, R.raw.obj_7, R.raw.obj_8,
-            R.raw.obj_9, R.raw.obj_10, R.raw.obj_11, R.raw.obj_12, R.raw.obj_13, R.raw.obj_14, R.raw.obj_15, R.raw.obj_16, R.raw.obj_17,
-            R.raw.obj_18, R.raw.obj_19, R.raw.obj_20, R.raw.obj_21, R.raw.obj_22, R.raw.obj_23, R.raw.obj_24, R.raw.obj_25, R.raw.obj_26,
-            R.raw.obj_27, R.raw.obj_28, R.raw.obj_29, R.raw.obj_30};
+    private int position;
+    private boolean cardFlipped = false;
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+    private int[] AnimalsSounds;
+    private int[] letterSounds;
+    private int[] letters;
+    private int[] animals;
+    private int[] animalsSpelling;
+    private int[] arabicLetterSpelling;
+    private int[] englishWords;
+    private int[] arabicWords;
 
-    private int[] letterSounds = {R.raw.letter_1, R.raw.letter_2, R.raw.letter_3, R.raw.letter_4, R.raw.letter_5, R.raw.letter_6, R.raw.letter_7, R.raw.letter_8,
-            R.raw.letter_9, R.raw.letter_10, R.raw.letter_11, R.raw.letter_12, R.raw.letter_13, R.raw.letter_14, R.raw.letter_15, R.raw.letter_16, R.raw.letter_17,
-            R.raw.letter_18, R.raw.letter_19, R.raw.letter_20, R.raw.letter_21, R.raw.letter_22, R.raw.letter_23, R.raw.letter_24, R.raw.letter_25, R.raw.letter_26,
-            R.raw.letter_27, R.raw.letter_28, R.raw.letter_29, R.raw.letter_30};
-
-    private int[] letters = {R.drawable.letter_a, R.drawable.letter_b, R.drawable.letter_c, R.drawable.letter_d, R.drawable.letter_e, R.drawable.letter_f,
-            R.drawable.letter_g, R.drawable.letter_h, R.drawable.letter_i, R.drawable.letter_j, R.drawable.letter_k, R.drawable.letter_l
-            , R.drawable.letter_m, R.drawable.letter_n, R.drawable.letter_o, R.drawable.letter_p, R.drawable.letter_q, R.drawable.letter_r
-            , R.drawable.letter_s, R.drawable.letter_t, R.drawable.letter_u, R.drawable.letter_v, R.drawable.letter_w, R.drawable.letter_x
-            , R.drawable.letter_y, R.drawable.letter_z, R.drawable.letter_z2, R.drawable.letter_z3, R.drawable.letter_z4, R.drawable.letter_z5};
-
-    private int[] animals = {R.drawable.animal_a, R.drawable.animal_b, R.drawable.animal_c, R.drawable.animal_d, R.drawable.animal_e, R.drawable.animal_f
-            , R.drawable.animal_g, R.drawable.animal_h, R.drawable.animal_i, R.drawable.animal_j, R.drawable.animal_k, R.drawable.animal_l
-            , R.drawable.animal_m, R.drawable.animal_n, R.drawable.animal_o, R.drawable.animal_p, R.drawable.animal_q, R.drawable.animal_r
-            , R.drawable.animal_s, R.drawable.animal_t, R.drawable.animal_u, R.drawable.animal_v, R.drawable.animal_w, R.drawable.animal_x
-            , R.drawable.animal_y, R.drawable.animal_z, R.drawable.animal_z2, R.drawable.animal_z3, R.drawable.animal_z4, R.drawable.animal_z5};
-
-    private int[] animalsSpelling = {R.drawable.spelling_a, R.drawable.spelling_b, R.drawable.spelling_c, R.drawable.spelling_d, R.drawable.spelling_e, R.drawable.spelling_f
-            , R.drawable.spelling_c, R.drawable.spelling_h, R.drawable.spelling_i, R.drawable.spelling_j, R.drawable.spelling_k, R.drawable.spelling_l
-            , R.drawable.spelling_m, R.drawable.spelling_n, R.drawable.spelling_o, R.drawable.spelling_p, R.drawable.spelling_q, R.drawable.spelling_r
-            , R.drawable.spelling_s, R.drawable.spelling_t, R.drawable.spelling_u, R.drawable.spelling_v, R.drawable.spelling_w, R.drawable.spelling_x
-            , R.drawable.spelling_y, R.drawable.spelling_z, R.drawable.spelling_z2, R.drawable.spelling_z3, R.drawable.spelling_z4, R.drawable.spelling_z5};
-
-    private int[] arabicLetterSpelling = {R.string.alif, R.string.baa, R.string.taa, R.string.thaa, R.string.jeem,
-            R.string.haa, R.string.khaa, R.string.daal, R.string.dhall, R.string.raa, R.string.zaa, R.string.seen,
-            R.string.sheen, R.string.saad, R.string.dhaad, R.string.taa, R.string.zaa, R.string.ayn, R.string.ghayan,
-            R.string.faa, R.string.qaaf, R.string.kaaf, R.string.laam, R.string.meem, R.string.noon, R.string.waaw,
-            R.string.haa, R.string.laam_alif, R.string.hamza, R.string.yaa};
-
-    private int[] englishWords = {R.string.rabbit, R.string.duck, R.string.crocodile, R.string.fox, R.string.camel,
-            R.string.horse, R.string.sheep, R.string.bear, R.string.housefly, R.string.raccoon, R.string.giraffe,
-            R.string.fish, R.string.lion_cub, R.string.hawk, R.string.frog, R.string.peacock, R.string.antelope,
-            R.string.spider, R.string.crow, R.string.elephant, R.string.cat, R.string.dog, R.string.strok, R.string.goat,
-            R.string.tiger, R.string.rhino, R.string.hoopoe, R.string.llama, R.string.lion, R.string.dragonfly};
-
-    private int[] arabicWords = {R.string.arnab, R.string.batta, R.string.timsah, R.string.thalab, R.string.jamal,
-            R.string.hisan, R.string.kharoof, R.string.dob, R.string.thababah, R.string.racoown, R.string.zarraffah,
-            R.string.samakah, R.string.shibl, R.string.saqar, R.string.dhafdaah, R.string.tawoos, R.string.dhaby,
-            R.string.anacaboot, R.string.goraab, R.string.feel, R.string.qitta, R.string.kalb, R.string.laq_laq,
-            R.string.maiz, R.string.namir, R.string.waheed, R.string.hoodhood, R.string.laamaa, R.string.asad, R.string.yassob};
     public CardContainerFragment() {
         setHasOptionsMenu(true);
     }
 
-    CardContainerFragment newInstance(int num) {
+    CardContainerFragment newInstance(int num, int[] AnimalsSounds, int[] letterSounds, int[] letters,
+                                      int[] animals, int[] animalsSpelling, int[] arabicLetterSpelling, int[] englishWords
+            , int[] arabicWords) {
         CardContainerFragment f = new CardContainerFragment();
         Bundle args = new Bundle();
         args.putInt("image", num);
+        args.putIntArray("AnimalsSounds",AnimalsSounds);
+        args.putIntArray("letterSounds",letterSounds);
+        args.putIntArray("letters",letters);
+        args.putIntArray("animals",animals);
+        args.putIntArray("animalsSpelling",animalsSpelling);
+        args.putIntArray("arabicLetterSpelling",arabicLetterSpelling);
+        args.putIntArray("englishWords",englishWords);
+        args.putIntArray("arabicWords",arabicWords);
         f.setArguments(args);
         return f;
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         position = getArguments().getInt("image", 0);
+        AnimalsSounds = getArguments().getIntArray("AnimalsSounds");
+        letterSounds = getArguments().getIntArray("letterSounds");
+        letters = getArguments().getIntArray("letters");
+        animals = getArguments().getIntArray("animals");
+        animalsSpelling = getArguments().getIntArray("animalsSpelling");
+        arabicLetterSpelling = getArguments().getIntArray("arabicLetterSpelling");
+        englishWords = getArguments().getIntArray("englishWords");
+        arabicWords = getArguments().getIntArray("arabicWords");
         View rootView = inflater.inflate(R.layout.fragment_card_container, container, false);
         final int commit = getChildFragmentManager()
                 .beginTransaction()
                 .add(R.id.container, new CardFrontFragment())
                 .commit();
-        if(true){
-            Random r = new Random();
-            for (int item = NBR_ITEMS - 1; item > 0; item--) {
-                int k = r.nextInt(item);
-                int e = AnimalsSounds[item];
-                AnimalsSounds[item] = AnimalsSounds[k];
-                AnimalsSounds[k] = e;
-                 e = letterSounds[item];
-                letterSounds[item] = letterSounds[k];
-                letterSounds[k] = e;
-                 e = letters[item];
-                letters[item] = letters[k];
-                letters[k] = e;
-                 e = animals[item];
-                animals[item] = animals[k];
-                animals[k] = e;
-                 e = animalsSpelling[item];
-                animalsSpelling[item] = animalsSpelling[k];
-                animalsSpelling[k] = e;
-                 e = arabicLetterSpelling[item];
-                arabicLetterSpelling[item] = arabicLetterSpelling[k];
-                arabicLetterSpelling[k] = e;
-                 e = englishWords[item];
-                englishWords[item] = englishWords[k];
-                englishWords[k] = e;
-                e = arabicWords[item];
-                arabicWords[item] = arabicWords[k];
-                arabicWords[k] = e;
-            }
-        }
         return rootView;
     }
 
@@ -196,38 +147,39 @@ public class CardContainerFragment extends android.app.Fragment {
 
         cardFlipped = !cardFlipped;
     }
-     @SuppressLint("ValidFragment")
-     public class CardFrontFragment extends android.app.Fragment {
 
-         public CardFrontFragment() {
-         }
+    @SuppressLint("ValidFragment")
+    public class CardFrontFragment extends android.app.Fragment {
 
-         private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-             @Override
-             public void onCompletion(MediaPlayer mediaPlayer) {
-                 // Now that the sound file has finished playing, release the media player resources.
-                 releaseMediaPlayer();
-             }
-         };
+        public CardFrontFragment() {
+        }
 
-         @Override
-         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                  Bundle savedInstanceState) {
-             View rootView = inflater.inflate(R.layout.fragment_front_layout, container, false);
-             ImageView imageView = rootView.findViewById(R.id.image_linear);
-             imageView.setImageResource(letters[position]);
-             TextView text = rootView.findViewById(R.id.arabic_spelling);
-             text.setText(arabicLetterSpelling[position]);
-             mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
-             rootView.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View v) {
-                     flipCard();
-                 }
-             });
-             return rootView;
-         }
-     }
+        private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                // Now that the sound file has finished playing, release the media player resources.
+                releaseMediaPlayer();
+            }
+        };
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_front_layout, container, false);
+            ImageView imageView = rootView.findViewById(R.id.image_linear);
+            imageView.setImageResource(letters[position]);
+            TextView text = rootView.findViewById(R.id.arabic_spelling);
+            text.setText(arabicLetterSpelling[position]);
+            mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+            rootView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    flipCard();
+                }
+            });
+            return rootView;
+        }
+    }
 
     @SuppressLint("ValidFragment")
     public class CardBackFragment extends android.app.Fragment {
--- app/src/main/java/com/example/android/alifbaa/SettingsActivity.java
+++ app/src/main/java/com/example/android/alifbaa/SettingsActivity.java
@@ -1,10 +1,7 @@
 package com.example.android.alifbaa;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
@@ -12,92 +9,54 @@ import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-
 public class SettingsActivity extends AppCompatActivity {
-    //sulaiman
-    @Override
+//sulaiman
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         // twitter button
-        final ImageView twitter = (ImageView) findViewById(R.id.twitter_icon);
+        final ImageView twitter = findViewById(R.id.twitter_icon);
         // email button
-        final ImageView email = (ImageView) findViewById(R.id.email_icon);
+        final ImageView email = findViewById(R.id.email_icon);
         // facebook button
-        final ImageView facebook = (ImageView) findViewById(R.id.facebook_icon);
+        final ImageView facebook = findViewById(R.id.facebook_icon);
         //home button
-        final CircleImageView home_button = findViewById(R.id.float_button_home);
+        final ImageView home_button = findViewById(R.id.float_button_home);
         // random order
-        final Switch switch1 = (Switch) findViewById(R.id.random_order);
+        final Switch switch1 = findViewById(R.id.random_order);
         // Lowercase letters
-        final Switch switch2 = (Switch) findViewById(R.id.Lowercase_letters);
+        final Switch switch2 = findViewById(R.id.Lowercase_letters);
         //enable swiping
-        final Switch switch3 = (Switch) findViewById(R.id.enable_swiping);
+        final Switch switch3 = findViewById(R.id.enable_swiping);
         // Show hints
-        final Switch switch4 = (Switch) findViewById(R.id.Show_hints);
+        final Switch switch4 = findViewById(R.id.Show_hints);
         // teacher mode
-        final Switch switch5 = (Switch) findViewById(R.id.teacher_mode);
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        if (preferences.contains("HINTS")) {
-            String displayHints = preferences.getString("HINTS", "ON");
-            if (displayHints.equals("ON"))
-                switch4.setChecked(true);
-            else
-                switch4.setChecked(false);
-        } else
-            switch4.setChecked(true);
-
-        if (preferences.contains("RANDOM")) {
-            String randomOrder = preferences.getString("RANDOM", "ON");
-            if (randomOrder.equals("ON"))
-                switch1.setChecked(true);
-            else
-                switch1.setChecked(false);
-        } else
-            switch1.setChecked(true);
-        // seekbar
-        SeekBar seekBar = (SeekBar) findViewById(R.id.id_seek_bar);
+        final Switch switch5 = findViewById(R.id.teacher_mode);
+        SeekBar seekBar = findViewById(R.id.id_seek_bar);
         facebook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
         });
-        // setOnClickListner for home button
         home_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
-                if (home_button.isClickable()) {
-                    Toast.makeText(SettingsActivity.this, "home button activiated", Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(SettingsActivity.this, " home button deactiviated", Toast.LENGTH_SHORT).show();
+           finish();
             }
         });
-        //  i used the setonCheckedChangeListner instead of the setOnclickListner
-        // setOnCheckedListner inherits compoundbutton attributes
+
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // ischecked switch1 ?
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
-                SharedPreferences.Editor editor = preferences.edit();
-
                 if (switch1.isChecked()) {
-                    editor.putString("RANDOM", "ON");
-                    Log.v("RANDOM","Are NOW ON");
-                }
-                else {
-                    editor.putString("RANDOM", "OFF");
-                    Log.v("RANDOM","Are NOW OFF");
-                }
-                editor.apply();
+                    Toast.makeText(SettingsActivity.this, "random order on", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(SettingsActivity.this, "random order off", Toast.LENGTH_SHORT).show();
             }
         });
         switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                // ischecked switch2 ?
                 if (switch2.isChecked()) {
                     Toast.makeText(SettingsActivity.this, " Lowercase letters on", Toast.LENGTH_SHORT).show();
                 } else
@@ -116,19 +75,10 @@ public class SettingsActivity extends AppCompatActivity {
         switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
-                SharedPreferences.Editor editor = preferences.edit();
-
                 if (switch4.isChecked()) {
-                    editor.putString("HINTS", "ON");
-                    Log.v("HINTS","Are NOW ON");
-                }
-                else {
-                    editor.putString("HINTS", "OFF");
-                    Log.v("HINTS","Are NOW OFF");
-
-                }
-                editor.apply();
+                    Toast.makeText(SettingsActivity.this, "hints is on ", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(SettingsActivity.this, "hints is off", Toast.LENGTH_SHORT).show();
             }
         });
         switch5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -145,16 +95,14 @@ public class SettingsActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 Toast.makeText(getApplicationContext(), "SeekBar progress: " + i, Toast.LENGTH_SHORT).show();
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 Toast.makeText(getApplicationContext(), "SeekBar touch started: ", Toast.LENGTH_SHORT).show();
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 Toast.makeText(getApplicationContext(), "SeekBar touch stopped: ", Toast.LENGTH_SHORT).show();
             }
         });
     }
-}
+    }
